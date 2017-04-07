8f026b10a973223a1df244b775877cfd40267d7f
Gulshan Kumar <k2caher@gmail.com>
2017-04-07 20:10:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/heartful/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hp.heartful;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SplashTime=7000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,SplashScreen.class);
+   startActivity(intent);
+                finish();
+            }
+        }, SplashTime);
+    }
+}
